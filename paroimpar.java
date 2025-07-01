7e7ef1e7bd0cb58d534dd0077a03f0bd784c9bc3
Seinarumo <santibra06@gmail.com>
2025-07-01 12:40:45 -0300

    Create paroimpar.java

---
+++ paroimpar.java
@@ -0,0 +1,10 @@
+public class ParOImpar {
+    public static void main(String[] args) {
+        int numero = 7; // Podés cambiar este valor por cualquier otro número entero
+
+        if (numero % 2 == 0)
+            System.out.println("El número es par");
+        else
+            System.out.println("El número es impar");
+    }
+}
